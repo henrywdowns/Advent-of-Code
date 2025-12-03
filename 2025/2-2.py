8a9c6612bd5be16ac7c3b21029de8b7a76fa885d
Henry <henrywdowns@gmail.com>
2025-12-02 21:25:08 -0500

    cleanup

--- 2025/2-2.py
+++ 2025/2-2.py
@@ -1,5 +1,4 @@
 from aocd_api import AOCD
-import math
 
 puzzle = AOCD(2,2025)
 
@@ -9,8 +8,7 @@ test_data = ['11-22','95-115','998-1012','1188511880-1188511890','222220-222224'
 '1698522-1698528','446443-446449','38593856-38593862','565653-565659',
 '824824821-824824827','2121212118-2121212124']
 
-# data = test_data
-
+# use split_equal() and factors() and see if any of the outputs are lists of identical values
 def compare_chunks(sequence):
     sequence = str(sequence)
     fs = factors(len(sequence))
@@ -19,10 +17,11 @@ def compare_chunks(sequence):
         if all([x == chunks[0] for x in chunks]):
             return True
     return False
-
+# take a number and split s into equal chunks
 def split_equal(s, size):
     return [s[i:i+size] for i in range(0, len(s), size)]
 
+# get all the factors that don't equal n, which i can use with split_equal()
 def factors(n):
     fs = []
     i = 1
